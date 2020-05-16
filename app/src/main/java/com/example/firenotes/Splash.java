b9fe6393be6095509576e02d08cb17ede75ba05e
MkItSimple <choanania1@gmail.com>
2020-05-16 16:32:36 +0800

    14-SpashScreen

---
+++ app/src/main/java/com/example/firenotes/Splash.java
@@ -0,0 +1,34 @@
+package com.example.firenotes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // check if user is logged in
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
